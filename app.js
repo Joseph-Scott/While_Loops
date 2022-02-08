3200443a3e20439080f246709344d130d0a92cd6
Joe Scott <jbs198585@gmail.com>
2022-02-08 14:09:37 -0500

    Write a guessing game

--- app.js
+++ app.js
@@ -12,9 +12,35 @@
 // }
 // console.log("CONGRATS YOU GOT THE SECRET!!!");
 
-let input = prompt("Hey, say something!");
-while (true) {
-  input = prompt(input);
-  if (input.toLowerCase() === "stop copying me") break;
+// let input = prompt("Hey, say something!");
+// while (true) {
+//   input = prompt(input);
+//   if (input.toLowerCase() === "stop copying me") break;
+// }
+// console.log("OK YOU WIN!");
+
+let maximum = parseInt(prompt("Enter the maximum number!"));
+while (!maximum) {
+  maximum = parseInt(prompt("Enter a valid number!"));
 }
-console.log("OK YOU WIN!");
+
+const targetNum = Math.floor(Math.random() * maximum) + 1;
+
+let guess = parseInt(prompt("Enter your first guess!"));
+let attempts = 1;
+
+while (parseInt(guess) !== targetNum) {
+  if (guess === "q") break;
+  attempts++;
+  if (guess > targetNum) {
+    guess = prompt("Too high! Enter a new guess:");
+  } else {
+    guess = prompt("Too low! Enter a new guess:");
+  }
+}
+if (guess === "q") {
+  console.log("Ok, YOU QUIT!");
+} else {
+  console.log("CONGRATS YOU WIN!");
+  console.log(`You got it! It took you ${attempts} guesses`);
+}
